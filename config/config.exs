423f8c3e89df03f313b4944d8cd9bc38c68e8597
4DA <4da@sandycat.info>
2020-05-20 21:44:05 +0300

    commit config.exs
    TODO: clear history after making public
    
    vanilla config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,14 @@
 use Mix.Config
 
 config :app,
-  bot_name: ""
+  bot_name: "INSTANCE_bot",
+  instance_url: "https://INSTANCE.club",
+  websocket_url: "wss://INSTANCE.club/api/v1/streaming",
+  instance_client_id: "INSTANCE_CLIENT_ID",
+  instance_client_secret: "INSTANCE_CLIENT_SECRENT",
+  register_link: "https://INSTANCE.club/registration"
 
 config :nadia,
-  token: ""
+  token: "BOT_TOKEN"
 
 import_config "#{Mix.env}.exs"
