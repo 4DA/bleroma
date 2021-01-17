43b711f260a16914f9fab5d768a5760481e39bbe
4DA <4da@sandycat.info>
2021-01-17 12:59:13 +0300

    remove "update from mastodon" logging

--- lib/app/matcher.ex
+++ lib/app/matcher.ex
@@ -36,7 +36,7 @@ defmodule Bleroma.Matcher do
   @impl true
   def handle_cast({:masto, tg_id, %{"event" => "update"} = message,
                    conn}, state) do
-    Logger.log(:info, "update from maston: #{inspect(message)}")
+    # Logger.log(:info, "update from maston: #{inspect(message)}")
     payload = Map.get(message, "payload")
     show_update(payload, tg_id, conn)
     {:noreply, state}
