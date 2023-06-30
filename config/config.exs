69364a98e934380ac44059f5b71b17a5c677f2f7
4DA <4da@sandycat.info>
2023-06-30 19:54:12 +0600

    [fix] use Config module

--- config/config.exs
+++ config/config.exs
@@ -1,12 +1,16 @@
-use Mix.Config
+import Config
 
 config :app,
-  bot_name: "INSTANCE_bot",
-  instance_url: "https://INSTANCE.club",
-  websocket_url: "wss://INSTANCE.club/api/v1/streaming",
+  bot_name: "birditytest_bot",
+  instance_url: "https://birdity.club",
+  websocket_url: "wss://birdity.club/api/v1/streaming",
   register_link: "https://INSTANCE.club/registration"
 
 config :nadia,
-  token: "BOT_TOKEN"
+  # token: "1182983412:AAHUMDIMPMSXW-AoWoXzQfupqg_hk73GOBw"
+  token: "888223960:AAF7ZNfHz_O1qYWx_Kh4AnuG4-Kouxb96I4"
+
+config :logger,
+  truncate: 65535
 
 import_config "#{Mix.env}.exs"
